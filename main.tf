ce0e918340cec04796a0f66e53784ad6208a4575
s poorna chandra <poornanaidu1@gmail.com>
2023-11-29 19:32:42 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
 module "components" {
-  source   = "git::https://github.com/SPOORNACHANDRA/tf-module-basic-test.git"
-  for_each = var.components
+  source   = "git::https://github.com/SPOORNACHANDRA/tf-module-vpc.git"
 
+  for_each = var.vpc
 
-  zone_id         = var.zone_id
-  security_groups = var.security_groups
-  name            = each.value["name"]
-  instance_type   = each.value["instance_type"]
+  cidr = each.value["cidr"]
 }
 
 
