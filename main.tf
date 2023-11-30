0067840d21b4101235a2e3345696e90c6d03293a
s poorna chandra <poornanaidu1@gmail.com>
2023-11-30 18:29:53 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-module "components" {
-  source   = "git::https://github.com/SPOORNACHANDRA/tf-module-vpc.git"
-
-  for_each = var.vpc
-
-  cidr = each.value["cidr"]
-}
-
+#module "components" {
+#  source   = "git::https://github.com/SPOORNACHANDRA/tf-module-vpc.git"
+#
+#  for_each = var.vpc
+#
+#  cidr = each.value["cidr"]
+#}
+#
 
 
 
